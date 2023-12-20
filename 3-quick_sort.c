e371e5f51f5a95e7391e1f1502b85ab777ca8120
EddV23 <edjevan40@gmail.com>
2023-12-20 01:27:35 +0000

    updated task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,7 +10,7 @@ int lomuto_partition(int *array, int low, int high, size_t size);
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	quick_sort_recursive(array, 0, size - 1, size);
